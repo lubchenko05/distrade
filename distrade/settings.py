fea8fda7fbb23b5e1bd51a43e122eedafad08db5
Yurij Lyubchenko <lubchenko05@gmail.com>
2017-10-21 19:01:41 +0300

    add static root

--- distrade/settings.py
+++ distrade/settings.py
@@ -137,6 +137,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
+PROJECT_DIR = os.path.dirname(__file__)
+
+STATIC_ROOT= os.path.join(PROJECT_DIR, 'static_media/')
 
 STATIC_URL = '/static/'
 
